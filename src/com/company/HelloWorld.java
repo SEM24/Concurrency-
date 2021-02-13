b5b68592946ba7d04e7796dda719dd934aed1a92
SEM <samerx777222@gmail.com>
2021-02-13 18:03:17 +0200

    Simple class to solve the task in method run3

---
+++ src/com/company/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class HelloWorld {
+    public void showHello() {
+        System.out.println("Hello");
+    }
+
+    public void showWorld() {
+        System.out.println("World");
+    }
+
+}
+
